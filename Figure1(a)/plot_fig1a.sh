6b8d1910c25b653d1acad737792a364bca07d3bc
Kuan Wang <kuanwang930203@gmail.com>
2024-01-13 17:04:17 -0600

    Update plot_fig1a.sh

--- Figure1(a)/plot_fig1a.sh
+++ Figure1(a)/plot_fig1a.sh
@@ -9,7 +9,7 @@ gmt gmtset FONT_ANNOT_PRIMARY  12p
 #PS=fault.ps
 PS=Fig1a.ps
 
-R=-R116.25/118.7/38.35/40.08
+R=-R116.2/118.7/38.35/40.08
 J=-JM20c
 I=-I0.001
 
@@ -34,7 +34,7 @@ coast=/project/wang/kuan/new_tianjin/plot/plot_total_subsidence/get_shp/tj_coast
 gmt psbasemap $R $J  -X21.5c -Bx0.5 -By0.5 -BWS -K  > $PS
 gmt psbasemap -R -J -K -O -Ben -B0 >> $PS
 
-gmt makecpt -Cgray -D -M -Z  > test.cpt
+# gmt makecpt -Cgray -D -M -Z  > test.cpt
 gmt grdimage $R $J gebco_2022.nc  -Ii_dem1.nc -Ctest.cpt -O -K >> $PS
 
 # gmt pscoast -R -J -B30g30 -Df -W0.5p -S176/226/255 -K -O >> $PS
@@ -44,7 +44,7 @@ gmt grdimage $R $J gebco_2022.nc  -Ii_dem1.nc -Ctest.cpt -O -K >> $PS
 # gmt psxy -R -J $hebei -W0.8p,black,solid -O -K >> $PS
 
 gmt psclip new_polygon.gmt -R -J -K -O >> $PS
-gmt grdimage lr_velocity_2016.0_2019.5.grd -R -J -Cmy4.cpt -Q -K -O >> $PS
+gmt grdimage lr_velocity_2016_2019.grd -R -J -Cmy4.cpt -Q -K -O >> $PS
 gmt psclip -C -K -O >> $PS
 
 gmt psxy -R -J $coast -W0.8p,black,solid -G176/226/255 -O -K >> $PS
@@ -58,19 +58,19 @@ echo 118.12 39.75 "P69 (Ascending)"|gmt pstext -R -K -J -F+f13p,1,255/128/128+a1
 gmt psxy  -R -J new_polygon.gmt -W1.2p,255/128/128 -O -K >> $PS
 
 #################################################################Wells##########################################################################
-awk '{print $1,$2}' GPSstationlist | gmt psxy -R -J -K -O -St0.3  -Gblue >> $PS
+awk '{print $1,$2}' GPSstationlist | gmt psxy -R -J -K -O -St0.3  -W0.01p,black -Gblue >> $PS
 awk '{print $1,$2,$3}' GPSstationlist | gmt pstext -R -J -K -O -F+f8p,5,black+jBC -D0/0.2 >> $PS
 
-awk '{print $1,$2}' GPSstationlist2 | gmt psxy -R -J -K -O -St0.3  -Gblue >> $PS
+awk '{print $1,$2}' GPSstationlist2 | gmt psxy -R -J -K -O -St0.3  -W0.01p,black -Gblue >> $PS
 awk '{print $1,$2,$3}' GPSstationlist2 | gmt pstext -R -J -K -O -F+f8p,5,black+jTC -D0/-0.2 >> $PS
 
-echo 117.88 39.56 | gmt psxy -R -J -K -O -St0.3  -Gblue >> $PS
+echo 117.88 39.56 | gmt psxy -R -J -K -O -St0.3  -Gblue -W0.01p,black >> $PS
 echo 117.88 39.56 CH02 | gmt pstext -R -J -K -O -F+f8p,5,black+jBC -D-0.50/-0.05 >> $PS
 
-echo 117.75 39.00  | gmt psxy -R -J -K -O -St0.3  -Gblue >> $PS
+echo 117.75 39.00  | gmt psxy -R -J -K -O -St0.3  -Gblue -W0.01p,black >> $PS
 echo 117.75 39.00  KC01 | gmt pstext -R -J -K -O -F+f8p,5,black+jBC -D-0.20/0.2 >> $PS
 
-echo 116.88 38.70  | gmt psxy -R -J -K -O -St0.3  -Gblue >> $PS
+echo 116.88 38.70  | gmt psxy -R -J -K -O -St0.3  -Gblue -W0.01p,black >> $PS
 echo 116.88 38.70  SW01 | gmt pstext -R -J -K -O -F+f8p,5,black+jBC -D0.10/0.2 >> $PS
 ##############################################################County names#######################################################################
 gmt pstext -R -J -Xa-0.75c -Ya-0.2c -F+f10p,1 -N  -O -K   << EOF   >> $PS
@@ -111,28 +111,33 @@ gmt pslegend -R -J -DjTL+w19.2c/0.55c+o0.12i/0.17i -F+gwhite@40+r -K -O >> $PS<<
 S 0.1i s 0.0000001 - 0.0p,- 0i
 EOF
 echo  "(a) InSAR-Derived 3.5-Year Land Surface Elevation Change Rate: 2016.0-2019.5"|gmt pstext -R -K -J -F+f12p,1,black+cTC -Dj0.2i/0.22i -O -N>> $PS
-echo  "Pixel size: 1.2-by-1.2 km"|gmt pstext -R -K -J -F+f11p,1,black+cBR -Dj0.35i/0.1i -O -N>> $PS
-echo 118.60 38.4	 "2" |gmt pstext  -J -R -K -O -F+f9p,1 >> $PS
+
+gmt pslegend -R -J -DjBR+w6.1c/2.8c+o0.1c/0.2c -F+gwhite@40+r -K -O >> $PS<< EOF
+S 0.1i s 0.0000001 - 0.0p,- 0i
+EOF
+
+echo 117.97 38.6 | gmt psxy -R -J -K -O -St0.5 -W0.05p,black -Gblue >> $PS
+echo "Continuous GPS Stations"|gmt pstext -R -K -J -F+f12p,1,black+cBR -Dj0.14i/0.96i -O -N>> $PS
+echo  "Pixel  size:  1.2-by-1.2 km"|gmt pstext -R -K -J -F+f12p,1,black+cBR -Dj0.29i/0.11i -O -N>> $PS
+echo 118.62 38.4	 "2" |gmt pstext  -J -R -K -O -F+f9p,1 >> $PS
 
 ##################################################################Color Bar#########################################################################
 gmt gmtset FONT_LABEL 10.5p
 gmt gmtset FONT_ANNOT_PRIMARY 10.5p
 ####################################################################################################################################################
 gmt psscale -R -J -DjBC+w6c/0.45c+o2.7i/0.5i+m -Cmy4.cpt -Bxa20f10 -K -O >> $PS
-echo 118.31 38.55	 "Elevation Change Rate (mm/yr)" |gmt pstext  -J -R -K -O -F+f12p >> $PS
+echo 118.31 38.55	 "Elevation Change Rate (mm/yr)" |gmt pstext  -J -R -K -O -F+f11p,1,black >> $PS
 
 #################################################################China Map###########################################################################
 gmt gmtset MAP_FRAME_PEN 0.5p
 ##############################################################Frame for small map####################################################################
 Rg=-R72/136/17/56
 Jg=-JM6c
-gmt psbasemap $Jg $Rg -Gwhite -Y12.3c -X0.01c -K -O -B0 >> $PS
+gmt psbasemap $Jg $Rg -Gwhite -Y12c -X0.01c -K -O -B0 >> $PS
 
 ##################################################################China boundary######################################################################
 gmt psxy bou1_4l.gmt -J -R -W0.5p -O -K >> $PS
 
-##################################################################Tianjin Frame#######################################################################
-gmt psxy  -R -J new_polygon.gmt -W1.3p,255/128/128 -O -K >> $PS
 ####################################################################Beijing Point#######################################################################
 gmt psxy -Sc0.1c -Gyellow -W0.5p,black -J -R -K -O << EOF >>$PS
 116.3 40.2
@@ -158,14 +163,14 @@ gmt psxy -R -J -K -O  S2N_Eastern_Shandong.xy  -W1p,brown >> $PS
 # gmt psxy -R -J -K -O S2N_Western_Route1.xy -W1p,green >> $PS
 # gmt psxy -R -J -K -O S2N_Western_Route2.xy -W1p,green >> $PS
 
-gmt pstext -R -J -K -O -F+f8p,1 << EOF >> $PS
-104 55 South-to-North Water Diversion Project
+gmt pstext -R -J -K -O -F+f8.5p,1 << EOF >> $PS
+104 54.9 South-to-North Water Diversion Project
 EOF
 #
 #
-gmt pstext -R -J -K -O -F+a0+jLM+f5p,1  << EOF >> $PS
-99 52 Central Route
-99 50.7 Easter Route
+gmt pstext -R -J -K -O -F+a0+jLM+f7p,1  << EOF >> $PS
+99 52.5 Central Route
+99 50.7 Eastern Route
 EOF
 # 95 52.3 Western Route (Planned)
 # gmt psxy -R -J -K -O  -W1.5p,green  << EOF >> $PS
@@ -173,42 +178,49 @@ EOF
 # 93 52.3
 # EOF
 gmt psxy -R -J -K -O  -W1.5p,red  << EOF >> $PS
-92 52
-97 52
+92 52.5
+97 52.5
 EOF
 gmt psxy -R -J -K -O  -W1.5p,brown  << EOF >> $PS
 92 50.7
 97 50.7
 EOF
+
+##################################################################Tianjin Frame#######################################################################
+gmt psxy  -R -J new_polygon.gmt -W0.8p,255/128/128 -Gyellow -O -K >> $PS
 #######################################################################Label names#########################################################################
-gmt pstext -R -J -K -O -F+f6p,1 << EOF >> $PS
-119 43.5 North China
-110 24.5   South China
-114.5 41.8 Beijing
+gmt pstext -R -J -K -O -F+f7p,1 << EOF >> $PS
+
+114.9 41.8 Beijing
 87 35.5  Qinghai-Tibet
 87 33  Plateau
 EOF
-
-gmt pstext -R -J -K -O -F+f5p,1   << EOF >> $PS
-125.4 38.3 Tianjin
+#119 43.5 North China
+#110 24.5   South China
+gmt pstext -R -J -K -O -F+f9p,1,red << EOF >> $PS
+124.7 39.3 Tianjin
 EOF
 
-gmt pstext -R -J -K -O -F+f5p,1+a8  << EOF >> $PS
+gmt pstext -R -J -K -O -F+f7p,1+a8  << EOF >> $PS
 112 28.3 Yangtze River
 EOF
 
-gmt pstext -R -J -K -O -F+f5p,1 << EOF >> $PS
-107.3 33.9  Han River
-EOF
+# gmt pstext -R -J -K -O -F+f5p,1 << EOF >> $PS
+# 107.3 33.9  Han River
+# EOF
 
-gmt pstext -R -J -K -O -F+f5p,1+a40   << EOF >> $PS
-103 39 Yellow River
+gmt pstext -R -J -K -O -F+f7p,1+a40   << EOF >> $PS
+102.9 39.1 Yellow River
 EOF
 ####################################################################Nansha Island#########################################################################
 R2=-R107/126/2.75/25
 J2=-JM1c
-gmt psbasemap $J2 $R2 -Gwhite -X5c -K -O -B0 >> $PS
-gmt psxy bou1_4l.gmt -J -R -W0.7p -O >> $PS
+gmt psbasemap $J2 $R2 -Gwhite -X0c -K -O -B0 >> $PS
+gmt psxy bou1_4l.gmt -J -R -W0.7p -K -O >> $PS
 
+gmt psbasemap $R $J  -X0c -Y-12c -Bx0.5 -By0.5 -Bws -K -O >> $PS
+gmt psbasemap -R -J -K -O -Ben -B0 >> $PS
+gmt psxy -R -J $out -W1p,black,solid -O  >> $PS
 
 gmt psconvert -A0.2c -Tf -E1000 $PS
+gmt psconvert -A0.2c -E1000 $PS
